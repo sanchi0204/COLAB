c37edeb1430e05d02f50dfe8aede927767a62b52
sanchi0204 <sanchirastogi02@gmail.com>
2021-04-10 01:24:12 +0530

    Update ProductFeedAdapter.java

--- Android/app/src/main/java/com/example/myntra/Adapters/ProductFeedAdapter.java
+++ Android/app/src/main/java/com/example/myntra/Adapters/ProductFeedAdapter.java
@@ -1,27 +1,37 @@
 package com.example.myntra.Adapters;
 
+import android.annotation.SuppressLint;
+import android.app.Activity;
 import android.content.Context;
+import android.content.Intent;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.CheckBox;
 import android.widget.ImageView;
 import android.widget.TextView;
-import android.widget.Toast;
 
 import androidx.annotation.NonNull;
+import androidx.cardview.widget.CardView;
 import androidx.recyclerview.widget.RecyclerView;
 
+import com.example.myntra.Model.Constants;
+import com.example.myntra.Model.ProductType;
 import com.example.myntra.R;
+import com.example.myntra.Views.AddToQueueDialog;
+import com.example.myntra.Views.ProdDesc;
 
 import java.util.ArrayList;
 
 public class ProductFeedAdapter extends RecyclerView.Adapter<ProductFeedAdapter.MyViewHolder> {
 
-    private final ArrayList products;
-    private Context context;
+    ArrayList<ProductType> productItem;
+    private final Context context;
+    private static final String TAG = "ProductFeedAdapter";
 
-    public ProductFeedAdapter(ArrayList products, Context context) {
-        this.products = products;
+
+    public ProductFeedAdapter(ArrayList<ProductType> productItem, Context context) {
+        this.productItem = productItem;
         this.context = context;
     }
 
@@ -30,51 +40,89 @@ public class ProductFeedAdapter extends RecyclerView.Adapter<ProductFeedAdapter.
     public MyViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
 
         View view = LayoutInflater.from(context).inflate(R.layout.content_product_feed, parent, false);
-        MyViewHolder myViewHolder = new MyViewHolder(view);
-        return myViewHolder;
+        return new MyViewHolder(view);
     }
 
+    @SuppressLint("SetTextI18n")
     @Override
     public void onBindViewHolder(@NonNull MyViewHolder holder, final int position) {
 
-        holder.brand.setText((CharSequence) products.get(position));
-        // implement setOnClickListener event on item view.
-        holder.itemView.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Toast.makeText(context, products.get(position)+" selected", Toast.LENGTH_SHORT).show();
+
+
+        holder.productLayout.setOnLongClickListener(view -> {
+            AddToQueueDialog addToQueueDialog = new AddToQueueDialog((Activity) context);
+            addToQueueDialog.show();
+            return true;
+        });
+
+
+        holder.wishlist.setOnCheckedChangeListener((compoundButton, b) -> {
+            if (holder.wishlist.isChecked() && position%2==0)
+            {
+                Constants.WishList.add(productItem.get(position).getProductId());
+                holder.recommendation.setText("As you wished for it");
             }
         });
-//        holder.speak.setOnClickListener(new View.OnClickListener() {
-//            @Override
-//            public void onClick(View view) {
-////                if (result == TextToSpeech.LANG_NOT_SUPPORTED || result == TextToSpeech.LANG_MISSING_DATA){
-////                    Toast.makeText(context, "Feature Not Support in your Device", Toast.LENGTH_SHORT).show();
-////                }else {
-////                    mTTS.speak(String.valueOf(numbers.get(position)), TextToSpeech.QUEUE_FLUSH, null);
-////                }
-//            }
-//        });
-    }
+
+        holder.cart.setOnCheckedChangeListener((compoundButton, b) -> {
+            if (holder.cart.isChecked() && position%2==0)
+            {
+                Constants.CartList.add(productItem.get(position).getProductId());
+                holder.recommendation.setText("Suits your cart history");
+            }
+        });
+
+        holder.like.setOnCheckedChangeListener((compoundButton, b) -> {
+            if (holder.like.isChecked() && position%2==0)
+            {
+               // Constants.WishList.add(productItem.get(position).getProductId());
+                holder.recommendation.setText("Trending");
+            }
+        });
+
+        String rating,brand,pid;
+        rating = String.valueOf(productItem.get(position).getRating());
+        pid = productItem.get(position).getProductId();
+
+
+        brand = productItem.get(position).getBrand();
+        holder.brand.setText(brand);
+
+        holder.productLayout.setOnClickListener(view -> {
+            Intent intent=new Intent(context, ProdDesc.class);
+            intent.putExtra("brand", brand);
+            intent.putExtra("rating", rating);
+            context.startActivity(intent);
+        });
+
+
+
+}
 
     @Override
     public int getItemCount() {
-        return products.size();
+        return productItem == null ? 0 : productItem.size();
     }
 
     public static class MyViewHolder extends RecyclerView.ViewHolder {
-        TextView brand,price,recommendation;
-        ImageView product;
+        TextView brand,recommendation;
+        ImageView productImg;
+        CheckBox like, dislike, cart, wishlist;
+        CardView productLayout;
 
         public MyViewHolder(@NonNull View itemView) {
             super(itemView);
 
             brand = itemView.findViewById(R.id.product_brand);
-            price = itemView.findViewById(R.id.product_price);
             recommendation = itemView.findViewById(R.id.product_recommendation);
-            product = itemView.findViewById(R.id.product_img);
+            productImg = itemView.findViewById(R.id.product_img);
 
+            like = itemView.findViewById(R.id.like);
+            dislike = itemView.findViewById(R.id.dislike);
+            cart = itemView.findViewById(R.id.cart_ic);
+            wishlist = itemView.findViewById(R.id.wishlist_ic);
 
+            productLayout = itemView.findViewById(R.id.card_product);
 
         }
     }
