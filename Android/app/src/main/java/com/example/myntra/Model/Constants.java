7b45b9d3b4bd69dddf56c85f8cf88b2d4b74ea94
sanchi0204 <sanchirastogi02@gmail.com>
2021-04-10 01:21:05 +0530

    Create Constants.java

---
+++ Android/app/src/main/java/com/example/myntra/Model/Constants.java
@@ -0,0 +1,25 @@
+package com.example.myntra.Model;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+
+public class Constants {
+
+    public static final ArrayList<String> WishList = new ArrayList<>();
+    public static final ArrayList<String> CartList = new ArrayList<>();
+    public static final ArrayList<String> friendList = new ArrayList<>();
+    public static final ArrayList<String> friendCart = new ArrayList<>();
+    public static final ArrayList<String> friendWish = new ArrayList<>();
+    public static final String RATING = "2";
+    public static final String PRODUCTID = "7482785221";
+    public static String NAME = "";
+    public static String EMAIL = "";
+    public static final ArrayList<User> userList = new ArrayList<>();
+    public static final ArrayList<PopulateRecommendation> recommendCart = new ArrayList<>();
+    public static final ArrayList<PopulateRecommendation> recommendWish = new ArrayList<>();
+
+   // public static List<String> productId = new ArrayList<>();
+
+
+}
